be0a98d67e8ddde72510f549e60b11699bbeac90
Elieudo Maia <elieudo.maia@g4flex.com.br>
2023-08-27 12:41:30 -0300

    chore: use path alias on tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,13 @@
 import { describe, expect, test } from 'vitest';
 
+import { TEST } from '@/teste';
+
 describe('test', () => {
   test('should pass', () => {
     expect(1 + 1).toBe(2);
+  });
+
+  test('should test with path alias', () => {
+    expect(TEST.isGood).toBe(true);
   });
 });
