0ab0c3a16ac0b673884d2e9e4a692fd2fcdb034f
Elieudo Maia <elieudo.maia@gmail.com>
2023-10-25 20:38:21 -0300

    chore: add database setup with prisma

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,28 @@
-import { TEST } from '@/teste';
+import { PrismaClient } from '@prisma/client';
 
-console.log('Hello World');
-console.log(TEST.isGood);
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      id: 'bba8bcae-f137-443a-a836-fbcfbdbe6104',
+      name: 'Teste',
+      email: 'teste@mail.com',
+      createdAt: new Date()
+    }
+  });
+
+  const users = await prisma.user.findMany();
+  console.dir(users, { depth: null });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+    process.exit(0);
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
---
+++ src/infrastructure/user/repository/user.repository.ts
@@ -0,0 +1,22 @@
+import { User } from '@/domain/user/entity/user';
+import {
+  FindUserByEmailRepository,
+  Input,
+  Output
+} from '@/domain/user/repository/find-user-by-email';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+export class UserRepository implements FindUserByEmailRepository {
+  async findUserByEmail(input: Input): Promise<Output> {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: input.email
+      }
+    });
+
+    if (!user) return null;
+
+    return new User(user.id, user.name, user.email, user.password);
+  }
+}
