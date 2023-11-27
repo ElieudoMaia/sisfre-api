750360d9d46012fde945a8e9874126d242ce8620
Elieudo Maia <elieudo.maia@gmail.com>
2023-11-27 20:47:47 -0300

    fix: corrigir nomes de arquivos

--- src/application/usecases/user/find-by-id/find-user-by-id.usecase.ts
+++ src/application/usecases/user/find-by-id/find-user-by-id.usecase.ts
@@ -1,14 +1,34 @@
-export type FindUserByIdInputDTO = {
-  id: string;
-};
+import { NotFoundError } from '@/domain/@shared/error/not-found.error';
+import { UseCase } from '@/domain/@shared/usecase/usecase.interface';
+import { FindUserByIdRepository } from '@/domain/user/repository/find-user-by-id';
+import {
+  FindUserByIdInputDTO,
+  FindUserByIdOutputDTO
+} from './find-user-by-id.usecase.dto';
 
-export type FindUserByIdOutputDTO = {
-  id: string;
-  name: string;
-  nameAbbreviation: string;
-  email: string;
-  role: string;
-  isActive: boolean;
-  createdAt: Date;
-  updatedAt: Date;
-};
+export class FindUserByIdUseCase
+  implements UseCase<FindUserByIdInputDTO, FindUserByIdOutputDTO>
+{
+  constructor(
+    private readonly findUserByIdRepository: FindUserByIdRepository
+  ) {}
+
+  async execute({ id }: FindUserByIdInputDTO): Promise<FindUserByIdOutputDTO> {
+    const user = await this.findUserByIdRepository.findUserById(id);
+
+    if (!user) {
+      throw new NotFoundError('User not found');
+    }
+
+    return {
+      id: user.id,
+      name: user.name,
+      nameAbbreviation: user.nameAbbreviation,
+      email: user.email,
+      role: user.role,
+      isActive: user.isActive,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt
+    };
+  }
+}
--- src/main/factories/usecases/user/find-user-by-id-usecase.factory.ts
+++ src/main/factories/usecases/user/find-user-by-id-usecase.factory.ts
@@ -1,4 +1,4 @@
-import { FindUserByIdUseCase } from '@/application/usecases/user/find-by-id/find-user-by-id.usecase.dto';
+import { FindUserByIdUseCase } from '@/application/usecases/user/find-by-id/find-user-by-id.usecase';
 import { UserRepository } from '@/infrastructure/user/repository/user.repository';
 
 export const makeFindUserByidUseCase = (): FindUserByIdUseCase => {
