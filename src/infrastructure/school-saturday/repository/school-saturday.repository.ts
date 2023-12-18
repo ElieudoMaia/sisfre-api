198c68a606ff3ba11dcb0adf878a92a91e40c67e
Elieudo Maia <elieudo.maia@gmail.com>
2023-12-17 21:01:49 -0300

    feat: implementar repositório de busca de sábado letivo pelo ID

--- src/infrastructure/school-saturday/repository/school-saturday.repository.ts
+++ src/infrastructure/school-saturday/repository/school-saturday.repository.ts
@@ -4,12 +4,16 @@ import {
 } from '@/domain/school-saturday/entity/school-saturday';
 import { CreateSchoolSaturdayRepository } from '@/domain/school-saturday/repository/create-school-saturday';
 import { FindSchoolSaturdayByDateRepository } from '@/domain/school-saturday/repository/find-school-saturday-by-date';
+import { FindSchoolSaturdayByIdRepository } from '@/domain/school-saturday/repository/find-school-saturday-by-id';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export class SchoolSaturdayRepository
-  implements CreateSchoolSaturdayRepository, FindSchoolSaturdayByDateRepository
+  implements
+    CreateSchoolSaturdayRepository,
+    FindSchoolSaturdayByDateRepository,
+    FindSchoolSaturdayByIdRepository
 {
   async create(input: SchoolSaturday): Promise<void> {
     await prisma.schoolSaturday.create({
@@ -28,6 +32,22 @@ export class SchoolSaturdayRepository
       where: {
         date
       }
+    });
+
+    if (!dbSchoolSaturday) return null;
+
+    return new SchoolSaturday({
+      id: dbSchoolSaturday.id,
+      dayOfWeek: dbSchoolSaturday.day_of_week as DayOfWeek,
+      date: dbSchoolSaturday.date,
+      createdAt: dbSchoolSaturday.created_at,
+      updatedAt: dbSchoolSaturday.updated_at
+    });
+  }
+
+  async findById(id: string): Promise<SchoolSaturday | null | undefined> {
+    const dbSchoolSaturday = await prisma.schoolSaturday.findUnique({
+      where: { id }
     });
 
     if (!dbSchoolSaturday) return null;
