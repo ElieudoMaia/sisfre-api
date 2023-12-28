0b8f6a7cdd5d8d6c651c93c5d737083575d70256
Elieudo Maia <elieudo.maia@gmail.com>
2023-12-27 21:30:25 -0300

    chore: exemplos de primeiras documentações de rotas

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,8 @@
+import cors from '@fastify/cors';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
 import fastify from 'fastify';
 import setupRoutes from './routes';
-
-import cors from '@fastify/cors';
 
 const app = fastify({ logger: true });
 
@@ -9,6 +10,213 @@ app.register(cors, {
   origin: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
+});
+
+app.register(fastifySwagger, {
+  mode: 'dynamic',
+  openapi: {
+    info: {
+      title: 'SisFre API',
+      version: '1.0.0',
+      contact: {
+        name: 'SisFre API',
+        email: 'elieudo.maia@gmail.com',
+        url: 'https:sisfre.ifce.edu.br'
+      },
+      description:
+        'API para o sistema de gerenciamento de frequencia do IFCE Campus Cedro'
+    },
+    servers: [{ url: 'http://localhost:3030' }],
+    components: {
+      requestBodies: {
+        User: {
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/User'
+              }
+            }
+          }
+        }
+      },
+      responses: {
+        UnauthorizedError: {
+          description: 'Unauthorized',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/UnauthorizedError'
+              }
+            }
+          }
+        },
+        ServerError: {
+          description: 'Internal Server Error',
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/ServerError'
+              }
+            }
+          }
+        }
+      },
+      schemas: {
+        User: {
+          type: 'object',
+          properties: {
+            id: { type: 'string' },
+            name: { type: 'string' },
+            nameAbbreviation: { type: 'string' },
+            email: { type: 'string' },
+            isActive: { type: 'boolean' },
+            role: { type: 'string' },
+            createdAt: { type: 'string' },
+            updatedAt: { type: 'string' }
+          }
+        },
+        UnauthorizedError: {
+          type: 'object',
+          properties: {
+            statusCode: {
+              type: 'number'
+            },
+            error: {
+              type: 'string'
+            },
+            message: {
+              type: 'string'
+            }
+          }
+        },
+        ServerError: {
+          type: 'object',
+          properties: {
+            statusCode: {
+              type: 'number'
+            },
+            error: {
+              type: 'string'
+            },
+            message: {
+              type: 'string'
+            }
+          }
+        }
+      }
+    },
+    paths: {
+      '/users': {
+        get: {
+          tags: ['Users'],
+          summary: 'Buscar usuários',
+          description: 'Buscar os usuários cadastrados no sistema',
+          operationId: 'listUsers',
+          parameters: [
+            {
+              name: 'pageNumber',
+              in: 'query',
+              description: 'Página a ser exibida',
+              required: false,
+              schema: {
+                type: 'number'
+              }
+            },
+            {
+              name: 'pageSize',
+              in: 'query',
+              description: 'Tamanho da página',
+              required: false,
+              schema: {
+                type: 'number'
+              }
+            }
+          ],
+          responses: {
+            200: {
+              description: 'Success',
+              content: {
+                'application/json': {
+                  schema: {
+                    type: 'object',
+                    properties: {
+                      users: {
+                        type: 'array',
+                        items: {
+                          $ref: '#/components/schemas/User'
+                        }
+                      },
+                      quantity: { type: 'number' },
+                      pageNumber: { type: 'number' },
+                      pageSize: { type: 'number' }
+                    }
+                  }
+                }
+              }
+            },
+            500: {
+              description: 'Internal Server Error',
+              $ref: '#/components/responses/ServerError'
+            },
+            401: {
+              description: 'Unauthorized',
+              $ref: '#/components/responses/UnauthorizedError'
+            }
+          }
+        }
+      },
+      '/users/{id}': {
+        get: {
+          tags: ['Users'],
+          summary: 'Buscar usuário pelo ID',
+          description: 'Buscar um usuário cadastrado no sistema',
+          operationId: 'getUser',
+          parameters: [
+            {
+              name: 'id',
+              in: 'path',
+              description: 'ID do usuário',
+              required: true,
+              schema: {
+                type: 'string'
+              }
+            }
+          ],
+          responses: {
+            200: {
+              description: 'Success',
+              content: {
+                'application/json': {
+                  schema: {
+                    $ref: '#/components/schemas/User'
+                  }
+                }
+              }
+            },
+            500: {
+              description: 'Internal Server Error',
+              $ref: '#/components/responses/ServerError'
+            },
+            401: {
+              description: 'Unauthorized',
+              $ref: '#/components/responses/UnauthorizedError'
+            }
+          }
+        }
+      }
+    },
+    tags: [{ name: 'Users' }]
+  }
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+  uiConfig: {
+    docExpansion: 'list',
+    deepLinking: false,
+    displayRequestDuration: true,
+    layout: 'BaseLayout'
+  }
 });
 
 app.get('/health', async () => ({
