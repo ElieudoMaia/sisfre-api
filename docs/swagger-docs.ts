c44e4986558883f34395035f6af21bd3d74c33b3
Elieudo Maia <elieudo.maia@gmail.com>
2024-01-14 15:32:28 -0300

    docs: adicionar rota de login e schema de autorização nas rotas

--- docs/swagger-docs.ts
+++ docs/swagger-docs.ts
@@ -15,6 +15,7 @@ export const swaggerRoutesDocumentation: FastifyDynamicSwaggerOptions = {
         'API para o sistema de gerenciamento de frequencia do IFCE Campus Cedro'
     },
     servers: [{ url: 'http://localhost:3030' }],
+    security: [{ bearerAuth: [] }],
     components: {
       requestBodies: {
         User: {
@@ -238,6 +239,13 @@ export const swaggerRoutesDocumentation: FastifyDynamicSwaggerOptions = {
               type: 'string'
             }
           }
+        }
+      },
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT'
         }
       }
     },
@@ -818,6 +826,69 @@ export const swaggerRoutesDocumentation: FastifyDynamicSwaggerOptions = {
             401: {
               description: 'Unauthorized',
               $ref: '#/components/responses/UnauthorizedError'
+            }
+          }
+        }
+      },
+      '/login': {
+        post: {
+          security: [],
+          tags: ['Login'],
+          summary: 'Login na aplicação',
+          description:
+            'Entre com as credenciais para receber um token de acesso',
+          requestBody: {
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    email: { type: 'string' },
+                    password: { type: 'string' }
+                  },
+                  required: ['email', 'password']
+                }
+              }
+            }
+          },
+          responses: {
+            '200': {
+              description: 'Logado com sucesso',
+              content: {
+                'application/json': {
+                  schema: {
+                    type: 'object',
+                    properties: {
+                      accessToken: { type: 'string' }
+                    }
+                  }
+                }
+              }
+            },
+            '400': {
+              description: 'Não autorizado',
+              content: {
+                'application/json': {
+                  schema: {
+                    type: 'object',
+                    properties: {
+                      statusCode: { type: 'number' },
+                      error: {
+                        type: 'string',
+                        enum: ['Bad Request']
+                      },
+                      message: {
+                        type: 'string',
+                        enum: ['User not found', 'Password incorrect']
+                      }
+                    }
+                  }
+                }
+              }
+            },
+            '500': {
+              description: 'Erro interno no servidor',
+              $ref: '#/components/responses/ServerError'
             }
           }
         }
