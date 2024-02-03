9873cf38a332f92cc9687b1b01c1bd232aaa4f33
Elieudo Maia <elieudo.maia@gmail.com>
2024-02-03 10:04:33 -0300

    chore: adicionar implementação do repositório de atualizar feriado

--- src/infrastructure/day-off-school/repository/day-off-school.repository.ts
+++ src/infrastructure/day-off-school/repository/day-off-school.repository.ts
@@ -1,10 +1,13 @@
 import { DayOffSchool } from '@/domain/day-off-school/entity/day-off-school';
 import { CreateDayOffSchoolRepository } from '@/domain/day-off-school/repository/create-day-off-school';
+import { UpdateDayOffSchoolRepository } from '@/domain/day-off-school/repository/update-day-off-school';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export class DayOffSchoolRepository implements CreateDayOffSchoolRepository {
+export class DayOffSchoolRepository
+  implements CreateDayOffSchoolRepository, UpdateDayOffSchoolRepository
+{
   async create(dayOffSchool: DayOffSchool): Promise<void> {
     await prisma.dayOffSchool.create({
       data: {
@@ -14,6 +17,19 @@ export class DayOffSchoolRepository implements CreateDayOffSchoolRepository {
         dateBegin: dayOffSchool.dateBegin,
         dateEnd: dayOffSchool.dateEnd,
         created_at: dayOffSchool.createdAt,
+        updated_at: dayOffSchool.updatedAt
+      }
+    });
+  }
+
+  async update(dayOffSchool: DayOffSchool): Promise<void> {
+    await prisma.dayOffSchool.update({
+      where: { id: dayOffSchool.id },
+      data: {
+        description: dayOffSchool.description,
+        type: dayOffSchool.type,
+        dateBegin: dayOffSchool.dateBegin,
+        dateEnd: dayOffSchool.dateEnd,
         updated_at: dayOffSchool.updatedAt
       }
     });
