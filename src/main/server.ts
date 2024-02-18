c7a91d6bc5eff7072d104983fa337c0a82b507a2
Elieudo Maia <elieudo.maia@gmail.com>
2024-02-18 07:59:49 -0300

    chore: não iniciar aplicação caso o banco de dados não connecte

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,12 @@
 import server from '@/main/config/app';
+import { PrismaClient } from '@prisma/client';
 
 const PORT = Number(process.env.PORT ?? 3030);
 
-server.listen({ port: PORT, host: '0.0.0.0' });
+new PrismaClient()
+  .$connect()
+  .then(() => server.listen({ port: PORT, host: '0.0.0.0' }))
+  .catch((error) => {
+    server.log.error(error);
+    process.exit(1);
+  });
